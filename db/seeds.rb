383c4ce54c391ba3044322e4d838ce3d4cba1c92
jair-silva <jair.silva@pitzi.com.br>
2024-03-04 20:40:30 -0300

    Configure: file seed to create faker citizens

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+50.times do
+  municipe = Municipe.create(
+    name: Faker::JapaneseMedia::OnePiece.character,
+    cpf: Faker::IDNumber.brazilian_citizen_number,
+    cns: rand.to_s[2..16],
+    email: Faker::Internet.email,
+    birthdate: Faker::Date.birthday(min_age: 18, max_age: 95).strftime('%d/%m/%Y'),
+    phone: Faker::PhoneNumber.cell_phone_in_e164,
+    photo: Faker::Avatar.image,
+    status: Faker::Boolean.boolean
+  )
+  municipe.create_endereco(
+    zipcode: Faker::Address.zip_code,
+    street: Faker::Address.street_name,
+    number: Faker::Address.building_number,
+    complement: Faker::Address.secondary_address,
+    district: Faker::Address.community,
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr
+  )
+end
